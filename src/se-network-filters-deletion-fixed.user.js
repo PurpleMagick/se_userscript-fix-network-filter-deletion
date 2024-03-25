b5b69e8449c781c592c3cb481ab55de29494d5d5
PurpleMagick <PurpleMagick@users.noreply.github.com>
2024-03-25 16:00:07 +0200

    Update and rename se-network-filters-delettion-fixed.user.js to se-network-filters-deletion-fixed.user.js

--- src/se-network-filters-deletion-fixed.user.js
+++ src/se-network-filters-deletion-fixed.user.js
@@ -11,47 +11,49 @@
 // ==/UserScript==
 
 (function() {
+	main();
+
 	function main() {
 		//handle coming in with a delete ID set
 		const searchParams = new URLSearchParams(location.search);
 		if (searchParams.has("delete")) //delete and exit
 			return deleteFilter(createDeleteLinkFromFilterId(searchParams.get("delete")));
-			
+
 		const deleteButton = document.querySelector("#yes");
-		
+
 		//nothing to delete
 		if (!deleteButton)
 			return;
-		
+
 		const deleteLink = deleteButton.href;
-		
-		//set the ID to delete to allow opening in new tab/window 
+
+		//set the ID to delete to allow opening in new tab/window
 		deleteButton.href = createActionURL(deleteLink);
-		
+
 		//if directly clicked, just perform the deletion immediately
 		deleteButton.addEventListener("click", (e) => {
 			e.preventDefault();
 			deleteFilter(deleteLink);
 		});
 	}
-	
+
 	function deleteFilter(deleteLink) {
 		const firstFilter = document.querySelector("#sideBar .filters ul:nth-of-type(1) li:nth-child(1) .filter-name a");
-	
+
 		fetch(deleteLink, { redirect: "manual" })
 			.then(() => location.href = firstFilter.href);
 	}
-	
+
 	function createDeleteLinkFromFilterId(filterId) {
 		return `https://stackexchange.com/filters/delete/${filterId}`;
 	}
-	
+
 	function extractFilterIdFromDeleteLink(deleteLink) {
 		const url = new URL(deleteLink);
 		const pathSegments = url.pathname.split("/");
 		return pathSegments[pathSegments.length - 1];
 	}
-	
+
 	function createActionURL(deleteLink) {
 		const actionURL = new URL(location.href);
 		actionURL.searchParams.set("delete", extractFilterIdFromDeleteLink(deleteLink));
